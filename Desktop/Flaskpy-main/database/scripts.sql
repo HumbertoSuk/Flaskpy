7837bf47b8e2ac9ba0e37dfa398e94a47fc51cf2
July Suk <humbertoxd25@gmail.com>
2024-03-05 23:41:05 -0800

    beh

---
+++ Desktop/Flaskpy-main/database/scripts.sql
@@ -0,0 +1,178 @@
+-- Crear la base de datos "store"
+CREATE SCHEMA store;
+
+-- Conectar a la base de datos "store"
+USE store;
+
+
+
+-- Tablas --------------------------------------------------------------------------------------------------------------------------------------
+
+-- Crear la tabla "users"
+CREATE TABLE users (
+    id smallint unsigned NOT NULL AUTO_INCREMENT,
+    username varchar(20) NOT NULL,
+    password char(102) NOT NULL,
+    fullname varchar(50),
+    usertype tinyint NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- Tabla de productos
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    imagen VARCHAR(255) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+);
+
+
+
+
+
+-- Procedimientos -----------------------------------------------------------------------------------------
+
+-- Obtener usuarios
+DELIMITER //
+
+CREATE PROCEDURE obtener_usuarios()
+BEGIN
+    SELECT id, username, password, fullname, usertype FROM users;
+END //
+DELIMITER ;
+
+-- Obtener usuarios por id
+DELIMITER //
+CREATE PROCEDURE obtener_usuario_por_id(in usuario_id INT)
+BEGIN
+    SELECT * FROM users WHERE id = usuario_id;
+END //
+DELIMITER ;
+
+-- Agregar usuario
+DELIMITER //
+CREATE PROCEDURE sp_AddUser(IN pUserName VARCHAR(20), IN pPassword VARCHAR(102), IN pFullName VARCHAR(50), IN pUserType tinyint)
+BEGIN
+    DECLARE hashedPassword VARCHAR(255);
+    SET hashedPassword = SHA2(pPassword, 256);
+    
+    INSERT INTO users (username, password, fullname, usertype)
+    VALUES (pUserName, hashedPassword, pFullName, pUserType);
+END //
+DELIMITER ;
+
+
+-- Actualizar usuario 
+DELIMITER //
+CREATE PROCEDURE actualizar_usuario(
+    IN usuario_id INT,
+    IN username VARCHAR(20),
+    IN password VARCHAR(50),
+    IN fullname VARCHAR(50),
+    IN usertype TINYINT
+)
+BEGIN
+    DECLARE new_hashed_password CHAR(102);
+
+    -- Verifica si se proporciona una nueva contraseña
+    IF password IS NOT NULL THEN
+        -- Hash de la nueva contraseña
+        SET new_hashed_password = SHA2(password, 256);
+    ELSE
+        -- Mantén la contraseña existente
+        SET new_hashed_password = (SELECT password FROM users WHERE id = usuario_id);
+    END IF;
+
+    -- Actualiza la información del usuario
+    UPDATE users
+    SET username = username,
+        password = new_hashed_password,
+        fullname = fullname,
+        usertype = usertype
+    WHERE id = usuario_id;
+END //
+DELIMITER;
+
+
+-- Eliminar usuarios
+
+DELIMITER //
+
+CREATE PROCEDURE eliminar_usuario(in usuario_id INT)
+BEGIN
+    DELETE FROM users WHERE id = usuario_id;
+END //
+DELIMITER ;
+
+-- Obtener productos
+DELIMITER //
+CREATE PROCEDURE obtener_productos()
+BEGIN
+    SELECT * FROM productos;
+END //
+DELIMITER ;
+
+-- Obtener un producto especifico por id
+DELIMITER //
+
+CREATE PROCEDURE obtener_producto_por_id(in producto_id INT)
+BEGIN
+    SELECT * FROM productos WHERE id = producto_id;
+END //
+DELIMITER ;
+
+-- Agregar producto
+
+DELIMITER //
+
+CREATE PROCEDURE agregar_producto(in nombre VARCHAR(255), in imagen VARCHAR(255), in precio DECIMAL(10, 2))
+BEGIN
+    INSERT INTO productos (nombre, imagen, precio) VALUES (nombre, imagen, precio);
+END //
+DELIMITER ;
+
+-- actualizar un producto
+DELIMITER //
+
+CREATE PROCEDURE actualizar_producto(in producto_id INT, in nombre VARCHAR(255), in imagen VARCHAR(255), in precio DECIMAL(10, 2))
+BEGIN
+    UPDATE productos SET nombre = nombre, imagen = imagen, precio = precio WHERE id = producto_id;
+END //
+DELIMITER ;
+
+-- Eliminar un producto
+DELIMITER //
+
+CREATE PROCEDURE eliminar_producto(in producto_id INT)
+BEGIN
+    DELETE FROM productos WHERE id = producto_id;
+END //
+
+DELIMITER ;
+
+-- Procedimiento para veridicar contraseña 
+DELIMITER $$
+
+CREATE PROCEDURE `sp_verifyIdentity`(IN pUsername VARCHAR(20), IN pPlainTextPassword VARCHAR(20))
+BEGIN
+    DECLARE storedPassword VARCHAR(255);
+
+    SELECT password INTO storedPassword FROM users
+    WHERE username = pUsername COLLATE utf8mb4_unicode_ci;
+
+    IF storedPassword IS NOT NULL AND storedPassword = SHA2(pPlainTextPassword, 256) THEN
+        SELECT id, username, storedPassword, fullname, usertype FROM users
+        WHERE username = pUsername COLLATE utf8mb4_unicode_ci;
+    ELSE
+        SELECT NULL;
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+
+call sp_AddUser("admin","123","juan perez",1);
+call sp_AddUser("user","123","Usuario",2);
+call sp_verifyIdentity("admin","123");
